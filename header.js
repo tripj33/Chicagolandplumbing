5a70210bd59b974ae2e5a2f84d8ce2f8ba1d354e
jjjanik3@gmail.com <jjjanik3@gmail.com>
2020-04-13 14:16:44 -0500

    Reverted to Previous working ver of mobile head

--- header.js
+++ header.js
@@ -4,78 +4,78 @@ $( document ).ready( function () {
     
     
 
-// ***Toggle Menu***
-
-// Menu button is clicked, function starts
-$("#btn-m-menu").click(function(){
-
-// Sets variable for menu state
-let menuState = $('.bottom').attr('data-menu');
-
-// Checks to see if menu state is closed
-if(menuState == 'menu-closed'){
- 
-    // Opens Menu 
-    $(".bottom").animate({marginLeft: "50vw"}, 500);
-
-    // Changes menu state attribute
-    $('.bottom').attr('data-menu', 'menu-open')
-
-}else{
-
-    // Closes Menu
-    $(".bottom").animate({marginLeft: "100vw"}, 500);
-
-    // Changes menu state attribute
-    $('.bottom').attr('data-menu', 'menu-closed')
-}
-
-});
-
-
-
-
-// ***Toggle Submenu***
-
-// Submenu is clicked, function starts
-$('.has-submenu').click(function(){
-
-let submenuState = $('.submenu').attr('data-submenu');
-
-let numberOfListItems = $('.submenu-item').length;
-let heightOfItems = $('.submenu-item').outerHeight(true) * numberOfListItems;
-let submenuHeight = heightOfItems + 'px';
-
-
-console.log(submenuHeight);
-
-// console.log($('.submenu-item'.outerHeight));
-// console.log(heightOfItems);
-// console.log(submenuHeight);
-
-
-
-if(submenuState == "submenu-closed"){
-    console.log("opening");
-    // Opens Submenu
-    $('.submenu').slideToggle(300);
-
-    // Changes submenu state attribute
-    $('.submenu').attr('data-submenu', 'submenu-open')
-    console.log("opening complete");
-}else{
-    console.log("closing");
-     // Closes Submenu
-     $('.submenu').slideToggle(300);
-
-
-     // Changes submenu state attribute
-     $('.submenu').attr('data-submenu', 'submenu-closed')
-     console.log("closing complete");
-}
-
-})
-
-
+    // ***Toggle Menu***
     
-});
+    // Menu button is clicked, function starts
+    $("#btn-m-menu").click(function(){
+    
+    // Sets variable for menu state
+    let menuState = $('.bottom').attr('data-menu');
+    
+    // Checks to see if menu state is closed
+    if(menuState == 'menu-closed'){
+     
+        // Opens Menu 
+        $(".bottom").animate({marginLeft: "50vw"}, 500);
+    
+        // Changes menu state attribute
+        $('.bottom').attr('data-menu', 'menu-open')
+    
+    }else{
+    
+        // Closes Menu
+        $(".bottom").animate({marginLeft: "100vw"}, 500);
+    
+        // Changes menu state attribute
+        $('.bottom').attr('data-menu', 'menu-closed')
+    }
+    
+    });
+    
+    
+    
+    
+    // ***Toggle Submenu***
+    
+    // Submenu is clicked, function starts
+    $('.has-submenu').click(function(){
+    
+    let submenuState = $('.submenu').attr('data-submenu');
+    
+    let numberOfListItems = $('.submenu-item').length;
+    let heightOfItems = $('.submenu-item').outerHeight(true) * numberOfListItems;
+    let submenuHeight = heightOfItems + 'px';
+    
+    
+    console.log(submenuHeight);
+    
+    // console.log($('.submenu-item'.outerHeight));
+    // console.log(heightOfItems);
+    // console.log(submenuHeight);
+    
+    
+    
+    if(submenuState == "submenu-closed"){
+        console.log("opening");
+        // Opens Submenu
+        $('.submenu').slideToggle(300);
+    
+        // Changes submenu state attribute
+        $('.submenu').attr('data-submenu', 'submenu-open')
+        console.log("opening complete");
+    }else{
+        console.log("closing");
+         // Closes Submenu
+         $('.submenu').slideToggle(300);
+    
+    
+         // Changes submenu state attribute
+         $('.submenu').attr('data-submenu', 'submenu-closed')
+         console.log("closing complete");
+    }
+    
+    })
+    
+    
+        
+    });
